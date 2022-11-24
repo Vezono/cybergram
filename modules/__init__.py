d51a1b0cd626e74fd550ce5eb550f4c9e23a924d
vezono <vezono.lab@gmail.com>
2022-11-24 00:26:57 -0500

    fix some lies

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,7 +2,7 @@ from pkgutil import iter_modules
 from importlib import import_module
 
 
-def load_modules(package: str = "src/modules"):
+def load_modules(package: str = "modules"):
     for _, name, _ in iter_modules([package]):
         path = f"{package.replace('/', '.')}.{name}"
         module = import_module(f"{package.replace('/', '.')}.{name}")

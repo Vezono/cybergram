92770d6797d72d9e0f0d7755392aad7144bc07b2
vezono <vezono.lab@gmail.com>
2022-11-29 10:45:01 -0500

    ev

--- modules/cybergram/__init__.py
+++ modules/cybergram/__init__.py
@@ -1,6 +1,6 @@
 from .reboot import RebootCommand
 from .schedule import ScheduleCommand
-from .exec_command import ExecCommand, AExecCommand
+from .exec_command import ExecCommand, AExecCommand, EvalCommand, AEvalCommand
 
-commands = [RebootCommand, ScheduleCommand, ExecCommand, AExecCommand]
+commands = [RebootCommand, ScheduleCommand, ExecCommand, AExecCommand, EvalCommand, AEvalCommand]
 listeners = []

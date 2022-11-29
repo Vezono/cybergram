338e170c56df41ca2e3a7791613b04b7b00794b9
vezono <vezono.lab@gmail.com>
2022-11-29 08:44:23 -0500

    git fix

--- src/utils/update.py
+++ src/utils/update.py
@@ -3,8 +3,9 @@ import pip
 import os
 
 def update():
-    g = git.cmd.Git('.')
-    g.pull()
+    g = git.cmd.Git('./')
+    print(g.pull())
+    return
     pip.main(['install', '-r', 'requirements.txt'])
     pip.main(['install', '-r', 'modules/requirements.txt'])
     modules = [f.path for f in os.scandir('modules') if f.is_dir()]

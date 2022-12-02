2fbca1c4a7525827ee761190df77c9ebf3027d11
vezono <vezono.lab@gmail.com>
2022-12-01 21:41:05 -0500

    ragna join

--- modules/ragna/__init__.py
+++ modules/ragna/__init__.py
@@ -2,4 +2,4 @@ from .storage import storage
 from .pve_handlers import *
 
 commands = [RagnaGoCommand]
-listeners = [PveStartListener, PveButtonListener, PveJoinListener, PveWinListener, PvePlayerListListener]
+listeners = [PveStartListener, PveButtonListener, PveJoinListener, PveWinListener, PvePlayerListListener, ChatJoinListener]

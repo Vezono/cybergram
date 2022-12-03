96f646858c182409e38054c6e29db0581ee8fc2a
vezono <vezono.lab@gmail.com>
2022-12-03 10:34:05 -0500

    role

--- modules/ragna/pve/priorities.py
+++ modules/ragna/pve/priorities.py
@@ -1,3 +1,3 @@
-NORMAL = ['deathsave', 'platform', 'standstill', 'concentrate', 'wakeup', 'puddle', 'role', 'subrole', 'in_answer', 'minions', 'jokes']
+NORMAL = ['deathsave', 'platform', 'standstill', 'concentrate', 'wakeup', 'puddle', 'in_answer', 'role', 'subrole', 'minions', 'jokes']
 OTREK = ['deathsave', 'platform', 'standstill', 'concentrate', 'wakeup', 'puddle', 'subrole', 'in_answer', 'minions', 'jokes', 'role']
 UNBUFFED = ['deathsave', 'platform', 'standstill', 'concentrate', 'wakeup', 'in_answer', 'jokes', 'puddle', 'role', 'subrole', 'minions']

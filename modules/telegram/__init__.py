8f42fe237873a0f44a8d0b0af0100b42c210081f
vezono <vezono.lab@gmail.com>
2022-12-03 22:57:34 -0500

    telegram module fix

--- modules/telegram/__init__.py
+++ modules/telegram/__init__.py
@@ -3,5 +3,5 @@ from .silent_chat_info_command import SilentChatInfoCommand
 from .silent_user_info_command import SilentUserInfoCommand
 from .ping_command import PingCommand
 
-commands = [MessageInfoCommand, SilentChatInfoCommand, SilentChatInfoCommand, PingCommand]
+commands = [MessageInfoCommand, SilentChatInfoCommand, SilentUserInfoCommand, PingCommand]
 listeners = []
